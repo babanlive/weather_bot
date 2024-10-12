99d00391aed3e76b36a05340273fec0a50307dc5
Mikhail Artemov <babanlive@gmail.com>
2024-10-12 18:08:04 +0700

    edited logging level

--- app/bot.py
+++ app/bot.py
@@ -22,5 +22,5 @@ async def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
